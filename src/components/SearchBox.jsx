b9bac806f6800db73445cde22d3ee0d170b9c09a
AlexTicovschi <alexmticovschi@gmail.com>
2018-10-14 17:57:17 +0100

    adding a search box component

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBox = ({ onSearchChange, getMovies }) => {
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={onFormSubmit}>
+      <input onChange={onSearchChange} type="text" name="search" placeholder="Search Movie..." />
+      <input onClick={getMovies} type="button" value="Search" />
+    </form>
+  );
+};
+
+export default SearchBox;
