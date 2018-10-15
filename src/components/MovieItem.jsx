82e55e45183dec14bf4ece691dd714dbf02c500b
AlexTicovschi <alexmticovschi@gmail.com>
2018-10-15 19:15:31 +0100

    getting movie info by id

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -7,7 +7,7 @@ const MovieItem = ({title, poster, id, getMovieById}) => {
             <h4 className="card-title">{title}</h4>
             <img src={poster === "N/A" ? "https://dummyimage.com/243x350/7b8a91/ffffff&text=Poster+Not+Available" : poster} alt="img card" />
             <div className="content">
-                <button className="btn">Movie Details</button>
+                <button onClick={() => getMovieById(id)} className="btn">Movie Details</button>
             </div>
         </article>
     );
--- src/containers/App.js
+++ src/containers/App.js
@@ -27,11 +27,22 @@ class App extends Component {
 
     const resp = await fetch(`http://www.omdbapi.com/?s=${this.state.search_term}&apikey=${APIKEY}`);
 
-        // Wait for the response and return as JSON
-        const movies = await resp.json();
+    // Wait for the response and return as JSON
+    const movies = await resp.json();
 
-        this.setState({ movies: movies.Search });
-        console.log('[MOVIE LIST]:',this.state.movies);
+    this.setState({ movies: movies.Search });
+    console.log('[MOVIE LIST]:',this.state.movies);
+  }
+
+  getMovieById = async (ID) => {
+    const APIKEY = '65480b96';
+    const resp = await fetch(`http://www.omdbapi.com/?i=${ID}&apikey=${APIKEY}`);
+
+    // Wait for the response and return as JSON
+    const movie = await resp.json();
+
+    this.setState({ movie });
+    console.log('[MOVIE]:',this.state.movie);
   }
 
   render() {
@@ -42,6 +53,7 @@ class App extends Component {
           getMovies={this.getMovies}/> 
         <MovieList 
           movieList={this.state.movies} 
+          getMovieById={this.getMovieById}
         />
       </div>
     );
