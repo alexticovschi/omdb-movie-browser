4ceeb74ce64f6c4f7c963a4581f9332408a1a7b3
AlexTicovschi <alexmticovschi@gmail.com>
2018-10-16 11:25:00 +0100

    saving movie list to local storage

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,11 +11,53 @@ class App extends Component {
     movies: []  
   }
 
+  componentDidMount(){
+    this.hydrateStateWithLocalStorage();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+    
+    // saves if component has a chance to unmount
+    this.saveStateToLocalStorage();
+  }
+
+  hydrateStateWithLocalStorage() {
+    // for all items in state
+    for (let key in this.state) {
+      // if the key exists in localStorage
+      if (localStorage.hasOwnProperty(key)) {
+        // get the key's value from localStorage
+        let value = localStorage.getItem(key);
+
+        // parse the localStorage string and setState
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          // handle empty string
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  }
+
+  saveStateToLocalStorage() {
+    // for every item in React state
+    for (let key in this.state) {
+      // save to localStorage
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  }
+
   onSearchChange = (event) => {
     event.preventDefault();
 
     this.setState({ search_term: event.target.value });
-    console.log('[searchField]:',this.state.search_term);
+    // console.log('[searchField]:',this.state.search_term);
   }
 
   getMovies = async () => {
@@ -27,18 +69,7 @@ class App extends Component {
     const movies = await resp.json();
 
     this.setState({ movies: movies.Search });
-    console.log('[MOVIE LIST]:',this.state.movies);
-  }
-
-  getMovieById = async (ID) => {
-    const APIKEY = '65480b96';
-    const resp = await fetch(`http://www.omdbapi.com/?i=${ID}&apikey=${APIKEY}`);
-
-    // Wait for the response and return as JSON
-    const movie = await resp.json();
-
-    this.setState({ movie });
-    console.log('[MOVIE]:',this.state.movie);
+    // console.log('[MOVIE LIST]:',this.state.movies);
   }
 
   render() {
